4081ad27967e7e9ad1461ea8f9648bb9305e4436
vijay2190 <vijayvinayak2190vv@gmail.com>
2024-01-20 20:26:27 +0530

    SortingPartice.x

---
+++ SortingPratice2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
+int partition(int *arr, int low, int high)
+{
+
+    int pivot = low, p = low + 1, q = high;
+
+    
+    while(p<=q)
+    {
+        while(arr[p] < arr[pivot])
+        {
+            p++;
+        }
+
+        while(arr[q] > arr[pivot])
+        {
+            q--;
+        }
+
+        if(p<q)
+        {
+            swap(&arr[p], &arr[q]); 
+        }
+               
+    }
+
+    swap(&arr[q], &arr[low]); 
+
+
+    return q;
+}
+
+
+int quickSort(int *arr, int low, int high)
+{
+    if(low < high)
+    {
+        int index = partition(arr,low, high);
+
+        quickSort(arr,low,index-1);
+        quickSort(arr, index+1, high);
+    }
+}
+
+int main()
+{
+     int arr[5] = {2, 8, 5, 3, 7};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+
+    quickSort(arr,0,size-1);
+
+    printArray(arr,size);
+
+
+    return 0;
+}
