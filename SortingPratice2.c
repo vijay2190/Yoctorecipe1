e91e7f46029fa88eac41d9c37f34c7d79582df47
vijay2190 <vijayvinayak2190vv@gmail.com>
2024-01-22 12:17:21 +0530

    onlyMergeSort

--- SortingPratice2.c
+++ SortingPratice2.c
@@ -22,54 +22,6 @@ int printArray(int *arr, int size)
 }
 
 
-int partition(int *arr, int low, int high)
-{
-
-    int pivot = low, p = low + 1, q = high;
-
-    
-    while(p<=q)
-    {
-        while(arr[p] < arr[pivot])
-        {
-            p++;
-        }
-
-        while(arr[q] > arr[pivot])
-        {
-            q--;
-        }
-
-        if(p<q)
-        {
-            swap(&arr[p], &arr[q]); 
-        }
-               
-    }
-
-    swap(&arr[q], &arr[low]); 
-
-
-    return q;
-}
-
-
-int quickSort(int *arr, int low, int high)
-{
-    if(low < high)
-    {
-        int index = partition(arr,low, high);
-
-        quickSort(arr,low,index-1);
-        quickSort(arr, index+1, high);
-    }
-}
-
-
-
-
-
-
 
 int mergeSort(int *arr, int size, int *lsa, int lsasize, int *rsa, int rsasize)
 {
@@ -150,12 +102,7 @@ int main()
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Quick Sort\n");
-
-    quickSort(arr,0,size-1);
-
-    printArray(arr,size);
-
+    
     printf("Merge Sort\n");
 
     merge_sort(arr,size);
